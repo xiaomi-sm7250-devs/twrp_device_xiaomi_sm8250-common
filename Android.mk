f92cecee6097cf683161f99f244d2de289dda18e
me-cafebabe <me_cafebabe@hotmail.com>
2022-05-27 18:47:29 +0800

    sm8250-common: Welcome mikona_a

--- Android.mk
+++ Android.mk
@@ -25,7 +25,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifeq ($(TARGET_DEVICE),$(filter $(TARGET_DEVICE),umi))
+ifeq ($(TARGET_DEVICE),$(filter $(TARGET_DEVICE),umi mikona_a))
     subdir_makefiles=$(call first-makefiles-under,$(LOCAL_PATH))
     $(foreach mk,$(subdir_makefiles),$(info including $(mk) ...)$(eval include $(mk)))
 endif
